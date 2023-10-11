c17dfc1d4f938659e4eb5e3e0688ea17d885a44b
Wendell Gabriel <wendell7gabriel@gmail.com>
2023-10-10 22:17:25 -0300

    private data users

--- src/main/java/br/com/wendellgabriel/todolist/user/UserController.java
+++ src/main/java/br/com/wendellgabriel/todolist/user/UserController.java
@@ -1,13 +1,16 @@
 package br.com.wendellgabriel.todolist.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
 
-
-    public 
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
 }
---
+++ src/main/java/br/com/wendellgabriel/todolist/user/UserModel.java
@@ -0,0 +1,9 @@
+package br.com.wendellgabriel.todolist.user;
+
+public class UserModel {
+
+    private String username;
+    public String name;
+    public String password;
+
+}
